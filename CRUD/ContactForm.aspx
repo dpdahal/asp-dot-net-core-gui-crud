abba3b2b1c9a1dfcf9055606845d2d1a30d523eb
dpdahal <dp48dahal@gmail.com>
2020-12-22 19:53:05 +0545

    first commit

---
+++ CRUD/ContactForm.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ContactForm.aspx.cs" Inherits="CRUD.ContactForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Name</td>
+                    <td>
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>
+                        <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>Phone</td>
+                    <td>
+                        <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnAddRecord" runat="server" Text="AddRecord" OnClick="btnAddRecord_Click" /> 
+                        <asp:Button ID="btnUpdate" runat="server" Text="Update Record" OnClick="btnUpdate_Click" /> 
+                         <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" /> 
+                    </td>
+                    
+
+                </tr>
+
+            </table>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3">
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ CRUD/ContactForm.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CRUD
+{
+    
+    public partial class ContactForm : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Data Source=DESKTOP-T5JPD0D;Initial Catalog=Students;Integrated Security=True";
+            con.Open();
+            if (!Page.IsPostBack)
+            {
+                DataShow();
+            }
+
+        }
+
+        protected void btnAddRecord_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Insert into student(Name,Email,Phone)values('"+txtName.Text.ToString()+ "','" + txtEmail.Text.ToString() + "','" + txtPhone.Text.ToString() + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+        public void DataShow()
+        {
+            ds = new DataSet();
+            cmd.CommandText = "Select * from student";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Delete from student where Name='" + txtName.Text.ToString() + "'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Update student Set Name='"+txtName.Text.ToString()+ "',Email='" + txtEmail.Text.ToString() + "',Phone='" + txtPhone.Text.ToString() + "' Where Name='"+txtName.Text.ToString()+"'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+    }
+}
